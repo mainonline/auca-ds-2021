49a45f5c884215ecd209abcb0ea5dbf1337fff7d
zetpages <akimov_z@auca.kg>
2021-10-03 16:37:56 +0200

    uva problem solving started

--- lab01/p03/main.cpp
+++ lab01/p03/main.cpp
@@ -6,13 +6,4 @@ int main()
 {
     string name;
     int grade;
-
-    while (cin >> name >> grade)
-    {
-        // for (int i = 0; i < grade; i++)
-        // {
-
-        // }
-        cout << name << " = " << grade * 2;
-    }
 }
--- uvatask/main.cpp
+++ uvatask/main.cpp
@@ -8,4 +8,25 @@ using namespace std;
 int main()
 {
     iostream::sync_with_stdio(false);
+
+    int n;
+    int a, b, c;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a >> b >> c;
+
+        if ((a > b && a < c) || (a < b && a > c))
+        {
+            cout << "Case " << i << ": " << a << endl;
+        }
+        else if ((b > a && b < c) || (b < a && b > c))
+        {
+            cout << "Case " << i << ": " << b << endl;
+        }
+        else
+        {
+            cout << "Case " << i << ": " << c << endl;
+        }
+    }
 }
