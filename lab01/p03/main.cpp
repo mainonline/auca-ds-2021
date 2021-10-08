8989248bf707821ffb03f6846aca0edde7eb8821
zetpages <akimov_z@auca.kg>
2021-10-08 12:12:25 +0200

    lab01 p03 completed

--- lab01/p03/main.cpp
+++ lab01/p03/main.cpp
@@ -1,9 +1,34 @@
+#include <iomanip>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    string name;
-    int grade;
+
+    const int kNameWidth = 30;
+    const int kAvgWidth = 40;
+    cout << setfill('.') << fixed;
+
+    for (string name; cin >> name;)
+    {
+        double sum = 0;
+        int nGrades = 0;
+
+        for (int grade; cin >> grade;)
+        {
+            sum += grade;
+            ++nGrades;
+        }
+        if (nGrades != 0)
+        {
+            cout << left << setw(kNameWidth) << name << " | " << right << setw(kAvgWidth) << setprecision(2) << sum / nGrades << "\n";
+        }
+        else
+        {
+            cout << left << setw(kNameWidth) << name << " | " << right << setw(kAvgWidth + 1) << "no data\n";
+        }
+        cin.clear();
+    }
 }
