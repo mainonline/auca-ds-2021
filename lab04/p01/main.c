e6c4aa09367d016fc1ba2bd299ba111c29ef7396
zetpages <akimov_z@auca.kg>
2021-12-20 13:47:08 +0100

    lab04p01

---
+++ lab04/p01/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    int y;
+    int *p;
+
+    x = 1;
+    p = &x;
+
+    printf("x = %d\n", x);
+    printf("x (*p) = %d\n", *p); // dereference operator
+    printf("address of x = %p\n", (void *)p);
+
+    *p = 2;
+
+    printf("x = %d\n", x);
+    printf("x (*p) = %d\n", *p);
+
+    y = *p * *p;
+
+    printf("y = %d\n", y);
+}
--- project1/main.cpp
+++ project1/main.cpp
@@ -1,116 +1,152 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// class BigInt
+// {
+// public:
+//   bool isNeg = false;
+//   string num = "0";
+
+//   BigInt() {}
+
+//   BigInt(const BigInt &other)
+//   {
+//     this->isNeg = other.isNeg;
+//     this->num = other.num;
+//   }
+
+//   BigInt(int num)
+//   {
+//     if (num < 0)
+//     {
+//       this->isNeg = true;
+//       num *= -1;
+//     }
+
+//     if (num == 0)
+//     {
+//       this->num = "0";
+//       return;
+//     }
+
+//     this->num = "";
+
+//     while (num > 0)
+//     {
+//       this->num += (num % 10) + '0';
+//       num /= 10;
+//     }
+//   }
+
+//   BigInt(const char *num)
+//   {
+//     string num_(num);
+//     *(this) = num_;
+//   }
+
+//   BigInt(string num)
+//   {
+//     while (num[0] == '-')
+//     {
+//       this->isNeg = !this->isNeg;
+//       num.erase(0, 1);
+//     }
+
+//     reverse(num.begin(), num.end());
+
+//     while (num.size() > 1 && num[num.size() - 1] == '0')
+//       num.erase(num.size() - 1, num.size());
+
+//     if (num == "")
+//       num = "0";
+
+//     this->num = num;
+
+//     if (num == "0")
+//       this->isNeg = false;
+//   }
+
+//   friend istream &operator>>(istream &in, BigInt &bigint)
+//   {
+//     string num;
+//     in >> num;
+//     bigint = move(num);
+//     return in;
+//   }
+
+//   friend ostream &operator<<(ostream &out, const BigInt &bigint)
+//   {
+//     string num = bigint.num;
+//     reverse(num.begin(), num.end());
+
+//     if (bigint.isNeg)
+//       num = '-' + num;
+
+//     out << num;
+
+//     return out;
+//   }
+
+//   friend bool operator==(const BigInt first, const BigInt second)
+//   {
+//     return first.num == second.num && second.isNeg == first.isNeg;
+//   }
+// };
+
+// int main()
+// {
+//   cout << "-- comparison --" << endl;
+
+//   // BigInt fBig("123123123123123123121231231231231243434343431");
+//   // BigInt sBig("12312312312312312312123123123123124343434343");
+//   BigInt fBig;
+//   BigInt sBig;
+
+//   cin >> fBig >> sBig;
+//   bool t = fBig == sBig;
+//   if (t)
+//   {
+//     cout << fBig << " -- == -- " << sBig << endl;
+//   }
+//   else
+//   {
+//     cout << fBig << " -- != -- " << sBig << endl;
+//   }
+// }
+
+// void addBigInteger(string a, string b)
+// {
+//   reverse(a.begin(), a.end());
+//   reverse(b.begin(), b.end());
+
+//   string c;
+//   int temp = 0;
+
+
+
+// }
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-class BigInt
-{
-public:
-  bool isNeg = false;
-  string num = "0";
-
-  BigInt() {}
-
-  BigInt(const BigInt &other)
-  {
-    this->isNeg = other.isNeg;
-    this->num = other.num;
-  }
-
-  BigInt(int num)
-  {
-    if (num < 0)
-    {
-      this->isNeg = true;
-      num *= -1;
-    }
-
-    if (num == 0)
-    {
-      this->num = "0";
-      return;
-    }
-
-    this->num = "";
-
-    while (num > 0)
-    {
-      this->num += (num % 10) + '0';
-      num /= 10;
-    }
-  }
-
-  BigInt(const char *num)
-  {
-    string num_(num);
-    *(this) = num_;
-  }
-
-  BigInt(string num)
-  {
-    while (num[0] == '-')
-    {
-      this->isNeg = !this->isNeg;
-      num.erase(0, 1);
-    }
-
-    reverse(num.begin(), num.end());
-
-    while (num.size() > 1 && num[num.size() - 1] == '0')
-      num.erase(num.size() - 1, num.size());
-
-    if (num == "")
-      num = "0";
-
-    this->num = num;
-
-    if (num == "0")
-      this->isNeg = false;
-  }
-
-  friend istream &operator>>(istream &in, BigInt &bigint)
-  {
-    string num;
-    in >> num;
-    bigint = move(num);
-    return in;
-  }
-
-  friend ostream &operator<<(ostream &out, const BigInt &bigint)
-  {
-    string num = bigint.num;
-    reverse(num.begin(), num.end());
-
-    if (bigint.isNeg)
-      num = '-' + num;
-
-    out << num;
-
-    return out;
-  }
-
-  friend bool operator==(const BigInt first, const BigInt second)
-  {
-    return first.num == second.num && second.isNeg == first.isNeg;
-  }
-};
-
 int main()
 {
-  cout << "-- comparison --" << endl;
+  string s = "23423423423";
+  // vector<char> v(s.begin(), s.end());
+  vector<char> v;
 
-  // BigInt fBig("123123123123123123121231231231231243434343431");
-  // BigInt sBig("12312312312312312312123123123123124343434343");
-  BigInt fBig;
-  BigInt sBig;
+  for(const char &c : s)
+  {
+    v.push_back(c);
+  }
 
-  cin >> fBig >> sBig;
-  bool t = fBig == sBig;
-  if (t)
+  int r = v[2] + v[3];
+  cout << r << endl;
+  for (int i = 0; i < v.size(); i++)
   {
-    cout << fBig << " -- == -- " << sBig << endl;
+    cout << v[i] << " ";
   }
-  else
-  {
-    cout << fBig << " -- != -- " << sBig << endl;
-  }
+  cout << endl;
 }
