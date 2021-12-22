f701a8721825815aff23362d0060e8d7ed27a609
zetpages <akimov_z@auca.kg>
2021-12-22 21:03:42 +0100

    pro01 uva01 wa

--- UVA/f-11321/main.cpp
+++ UVA/f-11321/main.cpp
@@ -1,23 +1,70 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int v;
+
+// bool ch(int a, int b)
+// {
+//     if (a % v != b % v)
+//         return a % v < b % v;
+//     if (a % 2 != 0)
+//     {
+//         if (b % 2 != 0)
+//             return a > b;
+//         return true;
+//     }
+//     if (b % 2 != 0)
+//         return false;
+//     return a < b;
+// }
+
+// int main()
+// {
+//     vector<int> vec;
+//     int n;
+//     cin >> n >> v;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int t;
+//         cin >> t;
+//         vec.push_back(t);
+//     }
+
+//     cout << n << " " << v << endl;
+
+//     sort(vec.begin(), vec.begin() + n, ch);
+
+//     for (int j = 0; j < n; j++)
+//     {
+//         cout << vec[j] << " " << endl;
+//     }
+// }
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int v;
 
-bool ch(int a, int b)
+struct IsEven
 {
-    if (a % v != b % v)
-        return a % v < b % v;
-    if (a % 2 != 0)
+    bool operator()(int a, int b) const
     {
+        if (a % v != b % v)
+            return a % v < b % v;
+        if (a % 2 != 0)
+        {
+            if (b % 2 != 0)
+                return a > b;
+            return true;
+        }
         if (b % 2 != 0)
-            return a > b;
-        return true;
+            return false;
+        return a < b;
     }
-    if (b % 2 != 0)
-        return false;
-    return a < b;
-}
+};
 
 int main()
 {
@@ -34,7 +81,7 @@ int main()
 
     cout << n << " " << v << endl;
 
-    sort(vec.begin(), vec.begin() + n, ch);
+    sort(vec.begin(), vec.begin() + n, IsEven());
 
     for (int j = 0; j < n; j++)
     {
